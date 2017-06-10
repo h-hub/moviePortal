8f0d231811352228e0aa2fb30bd8c12195ac9411
h-hub <harsha.kj89@gmail.com>
2017-06-11 03:39:02 +0800

    angular form fixes

--- src/main/webapp/angular2/src/app/movie/create-movie/create-movie.component.ts
+++ src/main/webapp/angular2/src/app/movie/create-movie/create-movie.component.ts
@@ -11,9 +11,27 @@ import { Movie } from '../movie/movie';
   styleUrls: ['/create-movie.component.css']
 })
 export class CreateMovieComponent {
-
+    
+    genres = [
+               {name:'Action', value:'Action', checked:false},
+               {name:'Adventure', value:'Adventure', checked:false},
+               {name:'Comedy', value:'Comedy', checked:false},
+               {name:'Crime', value:'Crime', checked:false},
+               {name:'Drama', value:'Drama', checked:false},
+               {name:'Fantasy', value:'Fantasy', checked:false},
+               {name:'Horror', value:'Horror', checked:false},
+             ]
+    
+    get selectedOptions() { // right now: ['1','3']
+        return this.genres
+                  .filter(opt => opt.checked)
+                  .map(opt => opt.value)
+      }
+    
   model = new Movie('', '', '', '', '', '');
 
   submitted = false;
   onSubmit() { this.submitted = true; }
+  
+  get diagnostic() { return JSON.stringify(this.model); }
 }
