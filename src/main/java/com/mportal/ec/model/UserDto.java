c40217511301509c17e1bef6ea8319daa7d2c66d
h-hub <harsha.kj89@gmail.com>
2017-06-30 20:22:14 +0800

    dto

--- src/main/java/com/mportal/ec/model/UserDto.java
+++ src/main/java/com/mportal/ec/model/UserDto.java
@@ -1,15 +1,15 @@
 package com.mportal.ec.model;
 
-import javax.validation.constraints.Max;
-import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
 
+import org.hibernate.validator.constraints.Length;
 import org.hibernate.validator.constraints.ScriptAssert;
 
 @ScriptAssert(lang = "javascript", script = "_this.password===_this.passwordRepeat")
 public class UserDto {
 	
-	@Min(6)
+	@Length(min=6)
+	@NotNull
 	private String username;
 	
 	@NotNull
@@ -18,9 +18,64 @@ public class UserDto {
 	@NotNull
 	private String emailRepeat;
 	
+	@Length(min=8)
 	@NotNull
 	private String password;
 	
+	@Length(min=8)
 	@NotNull
 	private String passwordRepeat;
+	
+	public UserDto() {
+    }
+
+	public UserDto(String username, String email, String emailRepeat, String password, String passwordRepeat) {
+		this.username = username;
+		this.email = email;
+		this.emailRepeat = emailRepeat;
+		this.password = password;
+		this.passwordRepeat = passwordRepeat;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getEmailRepeat() {
+		return emailRepeat;
+	}
+
+	public void setEmailRepeat(String emailRepeat) {
+		this.emailRepeat = emailRepeat;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getPasswordRepeat() {
+		return passwordRepeat;
+	}
+
+	public void setPasswordRepeat(String passwordRepeat) {
+		this.passwordRepeat = passwordRepeat;
+	}
+	
+	
 }
--- src/main/java/com/mportal/ec/web/UserController.java
+++ src/main/java/com/mportal/ec/web/UserController.java
@@ -1,20 +1,35 @@
 package com.mportal.ec.web;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.mportal.ec.model.User;
+import com.mportal.ec.model.UserDto;
+import com.mportal.ec.services.UserService;
 
 @RestController
 @RequestMapping(path="/users")
 public class UserController {
 	
-	@RequestMapping(method= RequestMethod.GET, path="/create" )
-	public User createUser(){
+	private UserService userService;
+	
+	@Autowired
+	public UserController(UserService userService) {
+		this.userService = userService;
+	}
+
+	@RequestMapping(method= RequestMethod.POST, path="/create" )
+	@ResponseStatus(HttpStatus.CREATED)
+	public void createUser(@RequestBody @Validated UserDto userDto ){
 		
+		userService.createUser(userDto.getUsername(), userDto.getEmail(), userDto.getPassword());
 		
-		
-		return null;
+//		return null;
 	}
 }
